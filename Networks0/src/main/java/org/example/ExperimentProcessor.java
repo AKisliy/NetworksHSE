d6e718a4875996a953628cfd0a48298da86ef3f2
AKisliy <lekha.a-kisel@yandex.ru>
2025-01-22 17:42:09 +0300

    Update Experiment Processor to use output stream directly (with write() and flush())

--- Networks0/src/main/java/org/example/ExperimentProcessor.java
+++ Networks0/src/main/java/org/example/ExperimentProcessor.java
@@ -10,8 +10,8 @@ public class ExperimentProcessor {
     private final int q;
     private final String outputFilePath;
 
-    private  BufferedReader in;
-    private PrintWriter out;
+    private BufferedReader in;
+    private OutputStream out;
 
     public ExperimentProcessor(int arraySizeMultiplier, int numberOfIterations, int numberOfNestedOperations, String outputFilePath){
         n = arraySizeMultiplier;
@@ -23,10 +23,9 @@ public class ExperimentProcessor {
     public void ProcessExperiment(Socket socket) throws IOException {
         try (
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true)
         ) {
             this.in = in;
-            this.out = out;
+            this.out = socket.getOutputStream();
             for (long i = 0; i < m; ++i) {
                 ProcessIteration(i);
             }
@@ -46,16 +45,17 @@ public class ExperimentProcessor {
         long curSize = n * iterationNum + 8;
         long curSum = 0;
         for (int j = 0; j < q; ++j){
-            char[] dataForServer = Generator.GenerateRandomString(curSize).toCharArray();
+            String dataForServer = Generator.GenerateRandomString(curSize);
             curSum += SendDataAndMeasureTime(dataForServer);
         }
         long averageTime = curSum / q;
         WriteResults(curSize, averageTime);
     }
 
-    private long SendDataAndMeasureTime(char[] data) throws IOException {
+    private long SendDataAndMeasureTime(String data) throws IOException {
         long start = System.currentTimeMillis();
-        out.println(data);
+        out.write((data + "\n").getBytes());
+        out.flush();
         // wait for response
         in.readLine();
         return System.currentTimeMillis() - start;
